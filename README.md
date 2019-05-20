2835855aedd062f74fa82172fd55cb42958bbc0c
Julio Lima <juliosergil@gmail.com>
2019-05-20 14:27:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rest-Inpi-Api
---
+++ src/main/java/com/inpi/RestInpiApiApplication.java
@@ -0,0 +1,13 @@
+package com.inpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInpiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestInpiApiApplication.class, args);
+	}
+
+}
